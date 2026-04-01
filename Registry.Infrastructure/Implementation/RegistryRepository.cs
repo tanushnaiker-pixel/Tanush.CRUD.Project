140156e2995ed7c48589df732dd99c9a01223f23
Tanush Naiker <TanushN@Betsoftware.com>
2026-04-01 10:12:33 +0200

    Testing

---
+++ Registry.Infrastructure/Implementation/RegistryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Registry.Core.Entities;
+using Registry.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+
+namespace Registry.Infrastructure.Implementation
+{
+    public class RegistryRepository: IRegistryRepository
+    {
+        private readonly IRegistryDBContext _dbContext;
+
+    }
+}
