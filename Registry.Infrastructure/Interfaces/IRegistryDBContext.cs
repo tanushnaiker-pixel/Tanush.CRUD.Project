1871fc63a1dee16662f1173fcde3ef2b10671cd2
Tanush Naiker <TanushN@Betsoftware.com>
2026-04-13 13:29:17 +0200

    Chore: Removing cancellation tokens

--- Registry.Infrastructure/Interfaces/IRegistryDBContext.cs
+++ Registry.Infrastructure/Interfaces/IRegistryDBContext.cs
@@ -10,8 +10,8 @@ namespace Registry.Infrastructure.Interfaces
 {
     public interface IRegistryDBContext
     {
-        Task<bool> ExecuteAsync(string query, DynamicParameters? queryParams = null, CancellationToken cancellationToken = default);
+        Task<bool> ExecuteAsync(string query, DynamicParameters? queryParams = null);
         Task<bool> IsHealthy();
-        Task<IEnumerable<T>?> QueryAsync<T>(string query, object? queryParams = null, CommandType? commandType = null, int? timeoutInSec = 30, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>?> QueryAsync<T>(string query, object? queryParams = null, CommandType? commandType = null, int? timeoutInSec = 30);
     }
 }
